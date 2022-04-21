d1c68d59fccf30258020c9b028987c67aad5b2af
Thomas McKernan <tmeaglei@gmail.com>
2022-04-20 23:18:28 -0700

    simpler

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ const {ModsSearchSortField} = require("node-curseforge/dist/objects/enums");
 
 let cf = new Curseforge.default(process.env['CURSEFORGE_API_KEY']);
 
-async function findMod(modId) {
-  for await (const mod of getAllMods()) {
-    if (mod.id !== modId) continue;
-    let m = mod.latestFiles[1];
-    let mostRecent = {
-      fileDate: Date.UTC(2000, 1)
-    };
-    for await (const file of getAllFiles(mod, m.sortableGameVersions.find(it => true).gameVersionTypeId)) {
-      mostRecent = mostRecent.fileDate > file.fileDate ? mostRecent : file
+async function findMod(modId, hash) {
+  for await (const file of getAllFiles(modId)) {
+    if (file.hashes.some(it => it.value === hash)) {
+      return await cf.get_file_download(modId, file.alternateFileId);
     }
-    return await cf.get_file_download(mod, mostRecent.alternateFileId);
   }
 }
 
@@ -32,28 +26,10 @@ async function* getAllFiles(mod, gameVersionTypeId) {
 }
 
 
-async function* getAllMods() {
-  let mc = await cf.get_game('minecraft');
-  let mods = await mc.search_mods({
-    sortField: ModsSearchSortField.TOTAL_DOWNLOADS,
-    categoryId: 4484,
-    sortOrder: "desc"
-  });
-  let totalPages = mods.paging.totalCount
-
-  for (let i = 0; i < totalPages; ++i) {
-    let mods = await mc.search_mods({
-      sortField: ModsSearchSortField.TOTAL_DOWNLOADS,
-      sortOrder: "desc",
-      categoryId: 4484,
-      index: i
-    });
-    console.error(`${i}/${totalPages}: MODS - ${mods.length} entries`)
-    yield* mods;
-  }
-}
-
-
-findMod(381671).then(m => {
-  console.log(m);
+const modPackId = parseInt(process.argv[2]);
+const hash = process.argv[3];
+console.error(`Finding modpack with Id: ${modPackId}, and hash ${hash}`)
+findMod(modPackId, hash).then(m => {
+  console.error(`Got most recent server pack ${m}`)
+  console.log(encodeURI(m));
 });
